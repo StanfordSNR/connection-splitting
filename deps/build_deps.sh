e589c9580e44a4ab29221a759c340279966e9b36
Gina Yuan <gina.yuan@gmail.com>
2024-11-12 14:18:22 -0700

    Script to build quic_client and quic_server binaries from chromium

--- deps/build_deps.sh
+++ deps/build_deps.sh
@@ -3,6 +3,7 @@ help() {
 	echo "USAGE: $0 [all|0|1]"
 	echo "0 = pepsal"
 	echo "1 = sidekick"
+	echo "2 = chromium"
 	exit 1
 }
 
@@ -11,6 +12,7 @@ if [ $# -ne 1 ]; then
 fi
 
 export SIDEKICK_HOME=$HOME/sidekick-downloads
+export PATH="$SIDEKICK_HOME/deps/depot_tools:$PATH"
 
 build_pepsal () {
 cd $SIDEKICK_HOME/deps/pepsal
@@ -28,13 +30,23 @@ cargo build --release
 cargo build --release --examples --all-features
 }
 
+build_chromium () {
+cd $SIDEKICK_HOME/deps/chromium/src
+gclient runhooks
+gn gen out/Default
+ninja -C out/Default quic_server quic_client
+}
+
 if [ $1 == "all" ]; then
 	build_pepsal
 	build_sidekick
+	build_chromium
 elif [ $1 -eq 0 ]; then
 	build_pepsal
 elif [ $1 -eq 1 ]; then
 	build_sidekick
+elif [ $1 -eq 2 ]; then
+	build_chromium
 else
 	help
 fi
--- deps/install_deps.sh
+++ deps/install_deps.sh
@@ -9,6 +9,7 @@ sudo apt-get update -y
 sudo apt-get install -y curl ethtool
 sudo apt-get install -y autoconf libtool  # curl
 sudo apt-get install -y libnfnetlink-dev  # pepsal
+sudo apt-get install -y libnss3-tools        # chromium
 sudo apt-get install -y mininet python3-pip  # mininet
 sudo apt-get install -y python3-virtualenv  # plotting
 
@@ -26,4 +27,33 @@ source $HOME/.cargo/env
 # Download external dependencies
 cd $SIDEKICK_HOME/deps
 git clone git@github.com:viveris/pepsal.git
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
+# Download chromium repository
+export PATH="$(pwd)/depot_tools:$PATH"
+update_depot_tools
+mkdir $SIDEKICK_HOME/deps/chromium
+cd $SIDEKICK_HOME/deps/chromium
+fetch --nohooks --no-history chromium
+
+# Install chromium dependencies
+cd $SIDEKICK_HOME/deps/chromium/src
+./build/install-build-deps.sh
+
+# Generate certificates
+cd $SIDEKICK_HOME/deps/chromium/src
+cd net/tools/quic/certs
+./generate-certs.sh
+cd -
+mkdir -p "$HOME/.pki/nssdb"
+certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n gquic -i net/tools/quic/certs/out/2048-sha256-root.pem
+openssl x509 -noout -pubkey < net/tools/quic/certs/out/leaf_cert.pem | \
+	openssl rsa -pubin -outform der | \
+	openssl dgst -sha256 -binary | \
+	openssl enc -base64
+
+# Setup cached webserver data
+mkdir -p /tmp/quic-data
+cd /tmp/quic-data
+wget -p --save-headers https://www.example.org
+sed -i "10i X-Original-Url: https://www.example.org/\r\n" "www.example.org/index.html"
