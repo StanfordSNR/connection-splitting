635a6d9aba381ac3ac51f875028d970b533719e9
Gina Yuan <gina.yuan@gmail.com>
2024-11-12 15:00:06 -0700

    Improve mininet popen interface for logging and background processes

--- emulation/common.py
+++ emulation/common.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import select
+import subprocess
 
 def TRACE(val):
     # LOG(val, 'TRACE')
@@ -20,13 +22,24 @@ def ERROR(val):
 def LOG(val, level):
     print(f'[SIDEKICK:{level}] {val}', file=sys.stderr);
 
-def popen(host, cmd):
-    p = host.popen(cmd.split(' '))
-    exitcode = p.wait()
-    for line in p.stderr:
-        sys.stderr.buffer.write(line)
-    if exitcode != 0:
-        print(f'{host}({cmd}) = {exitcode}')
-        sys.stderr.buffer.write(b'\n')
-        sys.stderr.buffer.flush()
-        exit(1)
+def popen(host, cmd, background=False, logger=TRACE, stdout=False, stderr=True):
+    p = host.popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if background:
+        logger(f'{host.name} {cmd} &')
+        return p
+    else:
+        logger(f'{host.name} {cmd}')
+        while p.poll() is None or p.stdout.peek() or p.stderr.peek():
+            ready, _, _ = select.select([p.stdout, p.stderr], [], [])
+            for stream in ready:
+                line = stream.readline()
+                if not line:
+                    continue
+                if stream == p.stdout and stdout:
+                    print(line.decode(), end='', file=sys.stdout)
+                if stream == p.stderr and stderr:
+                    print(line.decode(), end='', file=sys.stderr)
+        exitcode = p.wait()
+        if exitcode != 0:
+            print(f'{host}({cmd}) = {exitcode}', file=sys.stderr)
+            exit(1)
