6fd2f2c58fdda33153fdd6374e53be4d48c0c637
Gina Yuan <gina.yuan@gmail.com>
2025-01-06 16:16:14 -0500

    Add reno as a cca

--- emulation/main.py
+++ emulation/main.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
         help='Number of bytes to download in the HTTP/1.1 GET request, '\
              'e.g., 1000, 1K, 1M, 1000000, 1G')
     tcp.add_argument('-cca', '--congestion-control',
-        choices=['cubic', 'bbr', 'bbr2'], default='cubic',
+        choices=['reno', 'cubic', 'bbr', 'bbr2'], default='cubic',
         help='Congestion control algorithm at endpoints')
     tcp.add_argument('--pep', action='store_true',
         help='Enable PEPsal, a connection-splitting TCP PEP')
