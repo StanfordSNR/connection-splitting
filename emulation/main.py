a5610a0077703e79d15d797c124488bc6b01d317
Gina Yuan <gina.yuan@gmail.com>
2025-05-05 17:39:37 -0600

    Update CLI --help

--- emulation/main.py
+++ emulation/main.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     setLogLevel('info')
 
     parser = argparse.ArgumentParser(
-        prog='Sidekick',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     subparsers = parser.add_subparsers(required=True)
@@ -27,13 +26,13 @@ if __name__ == '__main__':
         help='Experiment timeout, in seconds')
     exp_config.add_argument('--label', type=str, default='NO_LABEL')
     exp_config.add_argument('--logdir', type=str, default='/tmp/atc25-logs',
-        help='Directory where host logs are written, in server.log and client.log')
+        help='Directory where host logs are written')
     exp_config.add_argument('--network-statistics', action='store_true',
         help='Include measured network statistics in experiment output')
     exp_config.add_argument('--topology',
-        choices=['one_hop', 'direct'], default='one_hop',
-        help='Network topology to use. If "direct", uses the network path '\
-             'properties for the "near path segment" i.e. Link 1.')
+        choices=['one_segment', 'two_segment'], default='two_segment',
+        help='Network topology to use. If "one_segment", uses the network '\
+             'path properties for the "near path segment" i.e. Link 1.')
     exp_config.add_argument('--pep', action='store_true',
         help='Enable PEPsal, a connection-splitting TCP PEP')
 
@@ -42,13 +41,13 @@ if __name__ == '__main__':
     ###########################################################################
     net_config = parser.add_argument_group('net_config')
     net_config.add_argument('--delay1', type=int, default=1, metavar='MS',
-        help='1/2 RTT on near path segment')
+        help='1/2 RTT on near path segment (to data receiver)')
     net_config.add_argument('--delay2', type=int, default=25, metavar='MS',
         help='1/2 RTT on far path segment')
     net_config.add_argument('--loss1', type=str, default='1', metavar='PERCENT',
         help='loss (in %%) on near path segment')
     net_config.add_argument('--loss2', type=str, default='0', metavar='PERCENT',
-        help='loss (in %%) on near path segment')
+        help='loss (in %%) on far path segment')
     net_config.add_argument('--bw1', type=int, default=100, metavar='MBPS',
         help='link bandwidth (in Mbps) on near path segment')
     net_config.add_argument('--bw2', type=int, default=10, metavar='MBPS',
@@ -145,12 +144,12 @@ if __name__ == '__main__':
     else:
         pacing = False
 
-    if args.topology == 'one_hop':
+    if args.topology == 'two_segment':
         net = TwoSegmentNetwork(args.delay1, args.delay2,
             args.loss1, args.loss2, args.bw1, args.bw2, args.qdisc, pacing)
         if args.pep:
             net.start_tcp_pep(logdir=args.logdir)
-    elif args.topology == 'direct':
+    elif args.topology == 'one_segment':
         assert not args.pep
         net = OneSegmentNetwork(args.delay1, args.loss1, args.bw1,
             args.qdisc, pacing)
